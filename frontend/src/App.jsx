ea7208b7aa436700a9104afb070450da601e0f14
Rajdeep Sen <rjdiips#gmail.com>
2025-10-17 22:05:44 +0530

    Implement cart functionality with add, update, and total calculation features; integrate toast notifications for user feedback.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -12,10 +12,12 @@ import Collection from "./pages/Collection";
 import About from "./pages/About";
 import Footer from "./components/Footer";
 import SearchBar from "./components/SearchBar";
+import { ToastContainer, toast } from "react-toastify";
 
 const App = () => {
   return (
     <div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]">
+      <ToastContainer />
       <Navbar />
       <SearchBar />
       <Routes>
--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -6,7 +6,7 @@ import RelatedProducts from "../components/RelatedProducts";
 
 const Product = () => {
   const { productId } = useParams();
-  const { products, currency } = useContext(ShopContext);
+  const { products, currency, addToCart } = useContext(ShopContext);
   const [productData, setProductData] = useState(false);
   const [image, setImage] = useState("");
   const [size, setSize] = useState("");
@@ -90,7 +90,10 @@ const Product = () => {
               ))}
             </div>
           </div>
-          <button className="bg-black text-white px-8 py-3 text-sm active:bg-gray-700 transition-colors duration-200 hover:bg-gray-800">
+          <button
+            onClick={() => addToCart(productData._id, size)}
+            className="bg-black text-white px-8 py-3 text-sm active:bg-gray-700 transition-colors duration-200 hover:bg-gray-800"
+          >
             ADD TO CART
           </button>
           <hr className="mt-8 sm:w-4/5 border-gray-200" />
